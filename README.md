6083e60a2678938e784715951fe4265cf06a1bf5
Waleed <waleedthedev@gmail.com>
2022-06-14 19:42:27 +0500

    Style guides setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Expenses Chart Component Solution | Frontend Mentor
+
+## Built with HTML + Tailwind CSS
+
+### Live Preview: [Expenses Chart Component]()
+
+### Challenge URL: [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt)
+
+---
+
+Author: [Waleed Mumtaz](https://twitter.com/WaleedMumtaaz)
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clr-soft-red': 'var(--clr-soft-red)',
+        'clr-cyan': 'var(--clr-cyan)',
+        'clr-dark-brown': 'var(--clr-dark-brown)',
+        'clr-medium-brown': 'var(--clr-medium-brown)',
+        'clr-cream': 'var(--clr-cream)',
+        'clr-very-pale-orange': 'var(--clr-very-pale-orange)',
+      },
+      fontFamily: {
+        'dm-sans': 'var(--ff-body)',
+      },
+      fontSize: {
+        'fs-body': 'var(--fs-body)',
+      },
+      fontWeight: {
+        'fw-400': 'var(--fw-400)',
+        'fw-700': 'var(--fw-700)',
+      },
+    },
   },
   plugins: [],
 }
